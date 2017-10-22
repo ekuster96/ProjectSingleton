6a64dd92e1bcf7051fecfc366c3a507ac4ba0fd2
ekuster96 <qster96@gmail.com>
2017-10-22 21:47:20 +0200

    Ok

--- src/eu/runelord/programmers/io/dp/singleton/Main.java
+++ src/eu/runelord/programmers/io/dp/singleton/Main.java
@@ -3,8 +3,21 @@ package eu.runelord.programmers.io.dp.singleton;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		SimpleSingleton s = new SimpleSingleton.getInstance();
+		SimpleSingleton ss = SimpleSingleton.getInstance();
+		SimpleSingleton ss1 = SimpleSingleton.getInstance();
+		SimpleSingleton ss2 = SimpleSingleton.getInstance();
+		int s1,s2,s3;
+		s1 = ss.hashCode();
+		s2 = ss1.hashCode();
+		s3 = ss2.hashCode();
+		if (s1 == s2 && s2 == s3)
+		{
+			System.out.print("Ta sama referencja");
+		}
+		
 	}
+	
+	
+	
 
 }
